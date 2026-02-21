df7c4061efedd872f98bf1bceed4490104d273af
Shaswath S <shaswaths@icloud.com>
2026-02-21 16:57:02 +0530

    NPP: dev paused

--- Network_Packet_Processing/Packet.cpp
+++ Network_Packet_Processing/Packet.cpp
@@ -3,9 +3,21 @@
 
 using namespace std;
 
+Packet::Packet(PacketType pType, Protocol prtcl) : type(pType), protocol(prtcl) {}
 
 string Packet::getPacketTypeName() const {
-
+    switch (type) {
+        case PacketType::UNKNOWN: return "Unknown";
+            break;
+        case PacketType::DATA: return "Data";
+            break;
+        case PacketType::CONTROL: return "Control";
+            break;
+        case PacketType::ERROR: return "Error";
+            break;
+            default: return "Unknown Error";
+    }
+    return "Unknown";
 }
 
 string Packet::getStatusName() const {
@@ -13,7 +25,17 @@ string Packet::getStatusName() const {
 }
 
 string Packet::getProtocolName() {
-
+    switch (protocol) {
+        case Protocol::TCP: return "TCP";
+            break;
+        case Protocol::UDP: return "UDP";
+            break;
+        case Protocol::HTTP: return "HTTP";
+            break;
+        case Protocol::FTP: return "FTP";
+            break;
+    }
+    return "Unknown";
 }
 
 void Packet::validatePacket() {
@@ -26,4 +48,8 @@ bool Packet::isError() {
 
 void Packet::displayInfo() {
 
+}
+
+void Packet::setType(PacketType newType) {
+    type = newType;
 }
--- Network_Packet_Processing/Packet.hpp
+++ Network_Packet_Processing/Packet.hpp
@@ -9,11 +9,15 @@ class Packet {
 private:
     string getPacketTypeName() const;
     string getStatusName() const;
-
+    PacketType type;
+    Protocol protocol;
+    Status packetStatus;
 
 public:
+    Packet(PacketType pType, Protocol prtcl);
     string getProtocolName();
     void validatePacket();
     bool isError();
     void displayInfo();
+    void setType(PacketType newType);
 };
